d5b45c4fcc9254c95ad7181185473a3440478160
Hunter van Geffen <h.c.vangeffen@student.tudelft.nl>
2021-05-26 12:24:14 +0200

    Fix styling

--- src/main/kotlin/com/github/huntervang/remla/HelloWorldAction.kt
+++ src/main/kotlin/com/github/huntervang/remla/HelloWorldAction.kt
@@ -4,7 +4,7 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
 
-class HelloWorldAction: AnAction() {
+class HelloWorldAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
         Messages.showMessageDialog(e.project, "Hello world!", "Hello Window", Messages.getInformationIcon())
