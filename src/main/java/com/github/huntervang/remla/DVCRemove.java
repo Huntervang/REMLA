951877260febb62c3012a8327d409ef9797b7320
Hunter van Geffen <h.c.vangeffen@student.tudelft.nl>
2021-06-10 21:17:23 +0200

    Add dvc remove button

---
+++ src/main/java/com/github/huntervang/remla/DVCRemove.java
@@ -0,0 +1,40 @@
+package com.github.huntervang.remla;
+
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import java.util.Objects;
+import org.jetbrains.annotations.NotNull;
+
+public class DVCRemove {
+    public static void dvcRemove(VirtualFile file, Project project) {
+        String message = Util.runConsoleCommand("dvc remove " + file.getPath(), project.getBasePath(), new ProcessAdapter() {
+            @Override
+            public void processTerminated(@NotNull ProcessEvent event) {
+                super.processTerminated(event);
+                // TODO: update UI when new files are dvc added
+            }
+        });
+
+        if (!Util.commandRanCorrectly(message)) {
+            // TODO: provide user feedback when add has not been succesful
+        }
+    }
+
+    public static void openDvcRemoveFilePicker(Project project) {
+        FileChooserDescriptor fileChooserDescriptor = new FileChooserDescriptor(true, false,
+                true, true, false, true);
+        fileChooserDescriptor.setRoots(ProjectRootManager.getInstance(project).getContentRootsFromAllModules());
+        fileChooserDescriptor.setTitle("Choose Files/Folders to Remove from Dvc");
+        fileChooserDescriptor.withFileFilter(virtualFile -> virtualFile.getPath().endsWith( ".dvc"));
+
+        VirtualFile[] files = FileChooser.chooseFiles(fileChooserDescriptor, project, null);
+        for (VirtualFile file : files) {
+            dvcRemove(file, project);
+        }
+    }
+}
