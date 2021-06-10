c0041c5846f847ddd4cb6f0e4570d68d0be544fa
tyarally <t.e.r.yarally@student.tudelft.nl>
2021-06-10 14:58:44 +0200

    Add google drive storage && open storage folder button

--- src/main/java/com/github/huntervang/remla/DVCAddRemote.java
+++ src/main/java/com/github/huntervang/remla/DVCAddRemote.java
@@ -2,19 +2,25 @@ package com.github.huntervang.remla;
 
 import com.intellij.execution.process.ProcessAdapter;
 import com.intellij.execution.process.ProcessEvent;
+import com.intellij.ide.BrowserUtil;
 import com.intellij.ide.actions.RevealFileAction;
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
 import com.intellij.openapi.fileEditor.impl.LoadTextUtil;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.util.Key;
 import com.intellij.openapi.vfs.LocalFileSystem;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.impl.status.widget.StatusBarWidgetWrapper;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.EventListener;
 
 public class DVCAddRemote {
     private JPanel dvcAddRemoteContent;
@@ -24,20 +30,30 @@ public class DVCAddRemote {
     private JRadioButton googleDriveRadioButton;
     private JButton openCurrentStorageButton;
     private final Project project;
+    private Icon defaultIcon;
 
     public DVCAddRemote(ToolWindow toolWindow) {
         project = Util.getProject();
+        defaultIcon = openCurrentStorageButton.getIcon();
         updateOpenStorageButton();
     }
 
     private void updateOpenStorageButton() {
         String storageLocation = getExistingRemote();
+        removeActionListeners(addStorageButton);
+        removeActionListeners(openCurrentStorageButton);
         if (storageLocation != null) {
-            openCurrentStorageButton.setVisible(true);
             addStorageButton.setText("Choose new storage location");
             addStorageButton.addActionListener(e -> chooseStorageLocation(true));
+            openCurrentStorageButton.setVisible(true);
             openCurrentStorageButton.setText("Current location: " + storageLocation);
-            openCurrentStorageButton.addActionListener(e -> RevealFileAction.openDirectory(new File(storageLocation)));
+            if (storageLocation.contains("gdrive")) {
+                openCurrentStorageButton.addActionListener(e -> BrowserUtil.browse("https://drive.google.com/drive/u/0/folders/" + storageLocation.split("gdrive://")[1]));
+                openCurrentStorageButton.setIcon(IconLoader.getIcon("/icons/gdrive_16x16.png"));
+            } else {
+                openCurrentStorageButton.addActionListener(e -> RevealFileAction.openDirectory(new File(storageLocation)));
+                openCurrentStorageButton.setIcon(defaultIcon);
+            }
         } else {
             addStorageButton.setText("Choose storage location");
             addStorageButton.addActionListener(e -> chooseStorageLocation(false));
@@ -45,10 +61,17 @@ public class DVCAddRemote {
         }
     }
 
+    private void removeActionListeners(JButton button) {
+        for (ActionListener actionListener : button.getActionListeners()) {
+            button.removeActionListener(actionListener);
+        }
+    }
+
     private String getExistingRemote() {
         try {
             VirtualFile file = LocalFileSystem.getInstance().findFileByPath(project.getBasePath() + "/.dvc/config");
             if (file != null) {
+                file.refresh(false, true);
                 CharSequence fileContent = LoadTextUtil.loadText(file);
                 String[] lines = fileContent.toString().split("\n");
                 for (int i = 0; i < lines.length; i++) {
@@ -76,11 +99,33 @@ public class DVCAddRemote {
             System.out.println("SSH storage!");
         } else if (googleDriveRadioButton.isSelected()) {
             System.out.println("Google Drive storage!");
+            String input = Messages.showInputDialog(project, "Please the ID of your Google Drive folder", "Enter Google Drive ID", Messages.getQuestionIcon());
+            if (input != null) {
+                addGDriveStorage(input, force);
+            }
         }
     }
 
+    private void addGDriveStorage(String gDriveId, boolean force) {
+        String response = Util.runConsoleCommand("dvc remote add --default data-storage gdrive://" + gDriveId + (force ? " --force" : ""), project.getBasePath(), new ProcessAdapter() {
+            @Override
+            public void onTextAvailable(@NotNull ProcessEvent event, @NotNull Key outputType) {
+                super.onTextAvailable(event, outputType);
+                String commandOutput = event.getText();
+                System.out.println(commandOutput);
+            }
+
+            @Override
+            public void processTerminated(@NotNull ProcessEvent event) {
+                super.processTerminated(event);
+                System.out.println("Command done!");
+                updateOpenStorageButton();
+            }
+        });
+    }
+
     private void addLocalStorage(VirtualFile dir, boolean force) {
-        String response = Util.runConsoleCommand("dvc remote add data-storage " + dir.getCanonicalPath() + (force ? " --force" : ""), project.getBasePath(), new ProcessAdapter() {
+        String response = Util.runConsoleCommand("dvc remote add --default data-storage " + dir.getCanonicalPath() + (force ? " --force" : ""), project.getBasePath(), new ProcessAdapter() {
             @Override
             public void onTextAvailable(@NotNull ProcessEvent event, @NotNull Key outputType) {
                 super.onTextAvailable(event, outputType);
