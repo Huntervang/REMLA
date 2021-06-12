e349de5ff59cb5fc89f7c950ade4a53c3b9dbf23
Hunter van Geffen <h.c.vangeffen@student.tudelft.nl>
2021-06-12 14:44:26 +0200

    Fix dvc check race condition

--- src/main/java/com/github/huntervang/remla/NoDVCProjectWindow.java
+++ src/main/java/com/github/huntervang/remla/NoDVCProjectWindow.java
@@ -5,36 +5,31 @@ import com.intellij.execution.process.ProcessEvent;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
-import com.intellij.openapi.wm.ToolWindow
-	;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import org.jetbrains.annotations.NotNull;
-
-import javax.swing.*;
-
-import static com.github.huntervang.remla.DVCToolWindowFactory.dvcToolWindowContent;
 
 public class NoDVCProjectWindow {
 	private JPanel noDVCProjectWindow;
 	private JButton initializeDVCProjectButton;
 
-	public NoDVCProjectWindow(ToolWindow toolWindow) {
-		initializeDVCProjectButton.addActionListener(e -> initializeProject(toolWindow));
+	public NoDVCProjectWindow(Project project, DVCToolWindowFactory dvcToolWindowFactory) {
+		initializeDVCProjectButton.addActionListener(e -> initializeProject(project, dvcToolWindowFactory));
 	}
 
-	private void initializeProject(ToolWindow toolWindow) {
+	private void initializeProject(Project project, DVCToolWindowFactory dvcToolWindowFactory) {
 		Util.runConsoleCommand("dvc init", Util.getProject().getBasePath(), new ProcessAdapter() {
 			@Override
 			public void processTerminated(@NotNull ProcessEvent event) {
-				Project currentProject = Util.getProject();
-				if (!Util.isGitInProject(currentProject.getBasePath())) {
-					ApplicationManager.getApplication().invokeLater(() -> Messages.showInfoMessage(currentProject,
+				if (!Util.isGitInProject(project.getBasePath())) {
+					ApplicationManager.getApplication().invokeLater(() -> Messages.showInfoMessage(project,
 						"This project is not a git project. If you want to setup a new project, run `git init` in your commandline",
 						"Not a Git Project"
 					));
 					return;
 				}
 				super.processTerminated(event);
-				ApplicationManager.getApplication().invokeLater(() -> toolWindow.getContentManager().setSelectedContent(dvcToolWindowContent));
+				ApplicationManager.getApplication().invokeLater(dvcToolWindowFactory::setDvcInProjectView);
 			}
 		});
 
