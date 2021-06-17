798030e4a5846a54e8983f40df675a36e4f90565
ahreurink <ahreurink@gmail.com>
2021-06-17 15:56:22 +0200

    Merge DVCDag and DVCRepro to avoid nullpointer exceptions

--- src/main/java/com/github/huntervang/remla/DVCDag.java
+++ src/main/java/com/github/huntervang/remla/DVCDag.java
@@ -1,31 +1,46 @@
 package com.github.huntervang.remla;
 
+import com.intellij.execution.process.ProcessAdapter;
+import com.intellij.execution.process.ProcessEvent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Key;
+import org.jetbrains.annotations.NotNull;
 import org.yaml.snakeyaml.Yaml;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public class DVCDag {
     private JPanel rootPanel;
     private JPanel dagPanel;
-    private JButton runPipelineButton;
     private final Project project;
     private final Pipeline pipeline;
 
-    public DVCDag(Project project) {
-        this.project = project;
-        this.pipeline = parseYaml();
+    private JButton reproButton;
+    private JList stageList;
+    private String activeStage = "";
+
+    private HashMap<String, Object> pipeLine;
+
+    public DVCDag(@NotNull Project newProject) {
+        project = newProject;
+        pipeline = parseYaml();
         if (this.pipeline != null) {
             drawDag();
         }
+        reproButton.addActionListener(e -> repro());
+        getPipeline(project.getBasePath() + "/dvc.yaml");
+    }
+
+    private void updateStage(String stageName){
+        activeStage = stageName;
+        System.out.println("setting: " + stageName);
     }
 
     private void drawDag() {
@@ -37,13 +52,19 @@ public class DVCDag {
                 break;
             }
         }
-        dagPanel.add(new JButton(current.getName()));
+        final String pipelineStage = current.getName();
+        JButton newButton = new JButton(pipelineStage);
+        newButton.addActionListener(e -> updateStage(pipelineStage));
+        dagPanel.add(newButton);
         while (true) {
             boolean hasNext = false;
             for (BuildStage next : pipeline.getStages()) {
                 if (!current.equals(next) && intersects(current.getOuts(), next.getDeps())) {
                     dagPanel.add(new JLabel("-->"));
-                    dagPanel.add(new JButton(next.getName()));
+                    final String pipelineStage2 = next.getName();
+                    newButton = new JButton(pipelineStage2);
+                    newButton.addActionListener(e -> updateStage(pipelineStage2));
+                    dagPanel.add(newButton);
                     current = next;
                     hasNext = true;
                     break;
@@ -95,6 +116,53 @@ public class DVCDag {
         }
         return null;
     }
+
+    private void getPipeline(String yamlFile){
+        Yaml DVCYaml = new Yaml();
+        File initialFile = new File(yamlFile);
+        try{
+            InputStream targetStream = new FileInputStream(initialFile);
+            pipeLine = DVCYaml.load(targetStream);
+        } catch(IOException e){
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void repro() {
+        System.out.println("Starting Reproduction");
+        System.out.println("active button: " + activeStage);
+        if (pipeLine != null) {
+            if(pipeLine.containsKey("stages")){
+                String dvcListCommand = "dvc repro " + activeStage; //repro only ./dvc.yaml to avoid deeper nested files
+                Vector<String> outputList = new Vector<>();
+                try {
+                    String response = Util.runConsoleCommand(dvcListCommand, project.getBasePath(), new ProcessAdapter() {
+                        @Override
+                        public void onTextAvailable(@NotNull ProcessEvent event, @NotNull Key outputType) {
+                            super.onTextAvailable(event, outputType);
+                            String commandOutput = event.getText();
+                            JLabel stageOutput = new JLabel();
+                            stageOutput.setText(commandOutput);
+                            outputList.add(commandOutput);
+                            stageList.setListData(outputList);
+                        }
+                    });
+
+                    if (!Util.commandRanCorrectly(response)) {
+                        System.out.println("Failed Reproduction");
+                        Util.errorDialog("Failed to Reproduce Pipeline", response);
+                    } else {
+                        System.out.println("Succeeded Reproduction");
+                    }
+                }catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+            }
+        } else {
+            Util.errorDialog("No Pipeline Setup","There is no pipeline detected. There should be a .yaml file in the DVC project.");
+        }
+    }
+
 }
 
 class Pipeline {
@@ -113,10 +181,10 @@ class Pipeline {
 }
 
 class BuildStage {
-    private String name;
-    private String cmd;
-    private List<String> deps;
-    private List<String> outs;
+    private final String name;
+    private final String cmd;
+    private final List<String> deps;
+    private final List<String> outs;
 
     public BuildStage(String name, String cmd, List<String> deps, List<String> outs) {
         this.name = name;
